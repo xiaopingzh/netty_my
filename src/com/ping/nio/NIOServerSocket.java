4df425b7ddc41dd9eb82795d65f665377500abff
zhangxiaoping <xiaoping6783@163.com>
2017-03-08 22:50:35 +0800

    thread reactor

--- src/com/ping/nio/NIOServerSocket.java
+++ src/com/ping/nio/NIOServerSocket.java
@@ -18,44 +18,66 @@ public class NIOServerSocket{
     
     public void initSocketServer() throws IOException{
     	//创建ServerSocketChannel并注册
-    	
 		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 		serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
 		serverSocketChannel.configureBlocking(false);
 		//在Selector上注册ServerSocketChannel
 		Selector selector = Selector.open();
-		SelectionKey  selectionKey = serverSocketChannel.register(selector, serverSocketChannel.validOps());
-		TCPProtocol protocol = new EchoSelectorProtocol(BUFSIZE);  
-		while(true){
-			if(selector.select(TIMEOUT) == 0){
-//				。。。。 do something
-				System.out.println(new Date().getTime());
-				continue;
-			}
-			Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
-			//循环取得集合中的每个键值  
-            while (keyIter.hasNext()){  
-                SelectionKey key = keyIter.next();   
-                //如果服务端信道感兴趣的I/O操作为accept  
-                if (key.isAcceptable()){  
-                    protocol.handleAccept(key);  
-                }  
-                //如果客户端信道感兴趣的I/O操作为read  
-                if (key.isReadable()){  
-                    protocol.handleRead(key);  
-                }  
-                //如果该键值有效，并且其对应的客户端信道感兴趣的I/O操作为write  
-                if (key.isValid() && key.isWritable()) {  
-                    protocol.handleWrite(key);  
-                }  
-                //这里需要手动从键集中移除当前的key  
-                keyIter.remove();   
-            }
-		}
+        new Thread(new ReactorTask(selector)).start();
+        //注册
+        SelectionKey  selectionKey = serverSocketChannel.register(selector, serverSocketChannel.validOps());
+
     }
     
     
     public static void main(String[] args) throws IOException {  
     	new NIOServerSocket().initSocketServer();
-    }  
+    }
+
+
+    /**
+     * 线程用于循环便利Selector监听注册的Channel上的事件
+     */
+    class ReactorTask implements Runnable{
+
+        private Selector selector;
+
+        public ReactorTask(Selector selector){
+            this.selector = selector;
+        }
+
+        @Override
+        public void run(){
+            try{
+                TCPProtocol protocol = new EchoSelectorProtocol(BUFSIZE);
+                while(true){
+                    if(this.selector.select(TIMEOUT) == 0){
+                        System.out.println(new Date().getTime());
+                        continue;
+                    }
+                    Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+                    //循环取得集合中的每个键值
+                    while (keyIter.hasNext()){
+                        SelectionKey key = keyIter.next();
+                        //如果服务端信道感兴趣的I/O操作为accept
+                        if (key.isAcceptable()){
+                            protocol.handleAccept(key);
+                        }
+                        //如果客户端信道感兴趣的I/O操作为read
+                        if (key.isReadable()){
+                            protocol.handleRead(key);
+                        }
+                        //如果该键值有效，并且其对应的客户端信道感兴趣的I/O操作为write
+                        if (key.isValid() && key.isWritable()) {
+                            protocol.handleWrite(key);
+                        }
+                        //这里需要手动从键集中移除当前的key
+                        keyIter.remove();
+                    }
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }  
